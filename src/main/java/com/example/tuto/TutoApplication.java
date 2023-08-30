4f55cad0fe8ca9e6d818bc1f272ebb70daa7aac1
Yoan15 <yoann.deco@gmail.com>
2023-08-30 20:05:43 +0200

    ajout studentController + getStudents endpoint

--- src/main/java/com/example/tuto/TutoApplication.java
+++ src/main/java/com/example/tuto/TutoApplication.java
@@ -2,22 +2,11 @@ package com.example.tuto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class TutoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutoApplication.class, args);
 	}
-
-	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
-	}
-
 }
